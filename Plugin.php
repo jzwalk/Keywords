4f7aea92d8110208cfc77a741a6e9f16ac642439
jzwalk <zyz59@126.com>
2017-01-06 22:27:27 +0800

    三年一更，重整新生……

--- Plugin.php
+++ Plugin.php
@@ -1,19 +1,19 @@
 <?php
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-
 /**
- * 自动为文章中出现的关键词添加链接
+ * 为文中的指定关键词添加链接
+ * 
  * @package Keywords
  * @author 羽中
- * @version 1.0.6
- * @dependence 13.12.12-*
- * @link http://www.jzwalk.com/archives/net/keywords-for-typecho
+ * @version 1.0.7
+ * @dependence 14.10.10
+ * @link http://www.yzmb.me/archives/net/keywords-for-typecho
  */
 class Keywords_Plugin implements Typecho_Plugin_Interface
 {
 	/**
 	 * 激活插件方法,如果激活失败,直接抛出异常
-	 *
+	 * 
 	 * @access public
 	 * @return void
 	 * @throws Typecho_Plugin_Exception
@@ -21,48 +21,55 @@ class Keywords_Plugin implements Typecho_Plugin_Interface
 	public static function activate()
 	{
 		Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('Keywords_Plugin','kwparse');
+		Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('Keywords_Plugin','kwparse');
 	}
 
 	/**
 	 * 禁用插件方法,如果禁用失败,直接抛出异常
-	 *
+	 * 
 	 * @static
 	 * @access public
 	 * @return void
 	 * @throws Typecho_Plugin_Exception
 	 */
-	public static function deactivate() {}
+	public static function deactivate(){}
 
 	/**
 	 * 获取插件配置面板
-	 *
+	 * 
 	 * @access public
 	 * @param Typecho_Widget_Helper_Form $form 配置面板
 	 * @return void
 	 */
 	public static function config(Typecho_Widget_Helper_Form $form)
 	{
-		$keywords = new Typecho_Widget_Helper_Form_Element_Textarea('keywords',NULL,'',_t('关键词链接'),_t('以“关键词”|(英文半角分隔号)“链接”形式填写, 每行一组. 如：<br/>google|http://www.google.com'));
-		$keywords->input->setAttribute('style','width:345px;height:150px;');
+		$keywords = new Typecho_Widget_Helper_Form_Element_Textarea('keywords',NULL,'',_t('关键词链接'),_t('以"关键词|(半角分隔号)链接"的形式填写, 每行一组<br/>例: google|http://www.google.com'));
+		$keywords->input->setAttribute('style','width:400px;height:150px');
 		$form->addInput($keywords);
-		$tagslink = new Typecho_Widget_Helper_Form_Element_Checkbox('tagslink',array('1'=>'自动替换'),NULL,_t('标签链接'),_t('文中若出现与本站标签相同的关键词则自动添加标签页链接'));
+
+		$tagslink = new Typecho_Widget_Helper_Form_Element_Checkbox('tagslink',array(1=>_t('自动替换')),NULL,_t('标签链接'),_t('将与本站标签相同的关键词自动替换为标签页链接'));
 		$form->addInput($tagslink);
-		$limits = new Typecho_Widget_Helper_Form_Element_Text('limits',NULL,'1',_t('链接频率'),_t('文中有多个重复关键词或标签时可限制链接替换次数'));
-		$limits->input->setAttribute('style','width:50px');
+
+		$limits = new Typecho_Widget_Helper_Form_Element_Text('limits',NULL,'1',_t('链接频次'),_t('文中有多个重复的关键词或标签时可限制链接次数'));
+		$limits->input->setAttribute('style','width:40px');
+		$limits->addRule('required',_t('链接次数不能为空'));
 		$form->addInput($limits->addRule('isInteger',_t('请填写整数数字')));
+
+		$pagelinks = new Typecho_Widget_Helper_Form_Element_Radio('pagelinks',array(1=>_t('是'),0=>_t('否')),1,_t('在页面使用'),_t('是否将以上的链接替换设置也作用于独立页面内容'));
+		$form->addInput($pagelinks);
 	}
 
 	/**
 	 * 个人用户的配置面板
-	 *
+	 * 
 	 * @access public
 	 * @param Typecho_Widget_Helper_Form $form
 	 * @return void
 	 */
-	public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
+	public static function personalConfig(Typecho_Widget_Helper_Form $form){}
 
 	/**
-	 * 关键词与标签替换
+	 * 执行数据替换
 	 * 
 	 * @access public
 	 * @param string $content
@@ -71,61 +78,21 @@ class Keywords_Plugin implements Typecho_Plugin_Interface
 	public static function kwparse($content,$widget,$lastResult)
 	{
 		$content = empty($lastResult) ? $content : $lastResult;
+		$keywords = self::keywords();
 
-		$db = Typecho_Db::get();
-		$settings = Helper::options()->plugin('Keywords');
-		$limit = $settings->limits;
+		if ($widget instanceof Widget_Archive && $keywords) {
+			$settings = Helper::options()->plugin('Keywords');
 
-		$tagselect = $db->select()->from('table.metas')->where('type = ?','tag');
-		$tagdata = $db->fetchAll($tagselect,array($widget->widget('Widget_Abstract_Metas'),'filter'));
-
-		//fix separator bug
-		$keywords = str_replace("\n","sep",$settings->keywords);
-		$kwsets = array_filter(explode('sep',$keywords));
-		$kwarray = array();
-		$tmpkwds = array();
-
-		//解析出关键词数组
-		if ($keywords && strpos($keywords,'|') !== 0) {
-			foreach ($kwsets as $kwset) {
-				$kwset = trim($kwset);
-				$kwarray[] = explode('|',$kwset);
+			//关闭页面替换
+			if ($widget->is('page') && !$settings->pagelinks) {
+				return $content;
 			}
-		}
 
-		//标签链接并入数组
-		if ($tagdata && $settings->tagslink) {
-			foreach ($tagdata as $tag) {
-				$tags[] = array($tag['name'],$tag['permalink']);
-			}
-			$kwarray = array_merge($kwarray,$tags);
-		}
-
-		if ($kwarray) {
-
-			//排序优先替换长词
-			usort($kwarray,array(new Keywords_Plugin,'sortdesc'));
-
-			foreach($kwarray as $i=>$row) {
-				list($kwd,$url) = $row;
-				for($j=$i+1;$j<count($kwarray);$j++) {
-					$subkwd = $kwarray[$j][0];
-
-					//包含短词则替换md5
-					if(strpos($kwd,$subkwd) !== false) {
-						$tmpkwd = '{'.md5($subkwd).'}';
-						$kwd = str_replace($subkwd,$tmpkwd,$kwd);
-						$tmpkwds[$tmpkwd] = $subkwd;
-					}
+			foreach($keywords as $i=>$row) {
+				if (false!==strpos($content,$row['0'])) {
+					$content = preg_replace('/(?!<[^>]*)'.$row['0'].'(?![^<]*(>|<\/[a|sc]))/s' //排除参数与链接
+					,'<a href="'.$row['1'].'" target="_blank" title="'.$row['0'].'">'.$row['0'].'</a>',$content,$settings->limits);
 				}
-
-				//不替换html内的词
-				$content = preg_replace('/(?!<[^>]*)('.$row[0].')(?![^<]*>)/s','<a href="'.$row[1].'" target="_blank" title="'.$kwd.'">'.$kwd.'</a>',$content,$limit);
-			}
-
-			//将短词md5替换回来
-			foreach($tmpkwds as $tmp=>$kwd) {
-				$content = str_replace($tmp,$kwd,$content);
 			}
 		}
 
@@ -133,15 +100,48 @@ class Keywords_Plugin implements Typecho_Plugin_Interface
 	}
 
 	/**
-	 * 按关键词长短排序
+	 * 输出关键词数据
 	 * 
-	 * @access public
-	 * @param array $a,$b
-	 * @return integer
+	 * @access private
+	 * @return array
 	 */
-	public static function sortdesc($a,$b)
+	private static function keywords()
 	{
-		return (strlen($a[0]) < strlen($b[0])) ? 1 : -1;
+		$settings = Helper::options()->plugin('Keywords');
+		$kwarray = array();
+		$keyword = trim(Typecho_Common::stripTags($settings->keywords));
+
+		if ($keyword && strpos($keyword,'|')) {
+			//解析关键词数组
+			$kwsets = array_filter(preg_split("/(\r|\n|\r\n)/",$keyword));
+			foreach ($kwsets as $kwset) {
+				$kwarray[] = explode('|',trim($kwset));
+			}
+		}
+
+		if ($settings->tagslink) {
+			$db = Typecho_Db::get();
+			$tagselect = $db->select()->from('table.metas')->where('type = ?','tag');
+			$tagdata = $db->fetchAll($tagselect,array(Typecho_Widget::widget('Widget_Abstract_Metas'),'filter'));
+
+			//并入标签链接
+			if ($tagdata) {
+				$tags = array();
+				foreach ($tagdata as $tag) {
+					$tags[] = array($tag['name'],$tag['permalink']);
+				}
+				$kwarray = array_merge($kwarray,$tags);
+			}
+		}
+
+		if ($kwarray) {
+			//优先处理长词
+			usort($kwarray,function($a,$b){
+				return (strlen($a['0']) < strlen($b['0'])) ? 1 : -1;
+			});
+		}
+
+		return $kwarray;
 	}
 
 }
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-### Keywords plugin for Typecho
-
-详细使用说明和效果演示见blog发布地址: 
-####http://www.jzwalk.com/archives/net/keywords-for-typecho
+### Typecho关键词链接插件Keywords
+2017年1月6日更新至**v1.0.7**: 
+- 重整代码提高效率
+- 修复影响链接及参数bug
+- 增加页面使用开关
+#### 详细说明与效果演示见blog发布地址: 
+ > http://www.yzmb.me/archives/net/keywords-for-typecho
